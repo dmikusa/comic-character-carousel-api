5dd8ccda47f3a929e0bec3a85c7618a23a7fccfe
Daniel Mikusa <dmikusa@pivotal.io>
2015-06-11 11:39:08 -0400

    Enabled per-instance configuration, for private api keys.

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask
 from flask import render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
+
 
 
 @app.route('/')
