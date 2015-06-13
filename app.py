1e9493458e599756ab613b99371e340f392afcef
Daniel Mikusa <dmikusa@pivotal.io>
2015-06-12 21:24:32 -0400

    Updated to use a decorator for the timer.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import uuid
 import requests
 import hashlib
 import logging
+import wrapt
 from timeit import default_timer
 from flask import Flask
 from flask import request
@@ -22,6 +23,14 @@ app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 
+@wrapt.decorator
+def time_request(wrapped, instance, args, kwargs):
+    start = default_timer()
+    r = wrapped(*args, **kwargs)
+    log.info('Request duration [%s]', (default_timer() - start))
+    return r
+
+
 def generate_auth_params():
     req_id = uuid.uuid1().hex
     req_data = req_id + app.config['PRIVATE_KEY'] + app.config['PUBLIC_KEY']
@@ -33,6 +42,7 @@ def generate_auth_params():
     }
 
 
+@time_request
 def list_characters(limit=20, offset=0, orderBy='name'):
     start = default_timer()
     params = generate_auth_params()
@@ -65,7 +75,6 @@ def list_characters(limit=20, offset=0, orderBy='name'):
                 c['thumbnail'] = "%s/portrait_uncanny.%s" % (
                     item['thumbnail']['path'], item['thumbnail']['extension'])
             result['cc'].append(c)
-        log.info('Result duration [%s]', (default_timer() - start))
         return result
     else:
         log.error('HTTP Code [%d]', r.status_code)
